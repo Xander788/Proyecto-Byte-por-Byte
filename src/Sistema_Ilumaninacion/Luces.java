f3121fc6c3dccc1d08a96d7f86a54155bb16f3f4
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-25 21:56:43 -0600

    Clase Luces y pakaje SistIluminacion
    Se crea el pakaje sistema_iluminacion con su clase base abstracta luces, la cual contiene sus atributos y metodos (get y encenderLuz)

---
+++ src/Sistema_Ilumaninacion/Luces.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema_Ilumaninacion;
+
+/**
+ *
+ * @author Gloriana
+ */
+public abstract class Luces {
+    //se establece una variable de control utilizable en todas las luces del auto
+    private boolean lucesActivas;
+
+    public boolean isLucesActivas() {
+        return lucesActivas;
+    }
+    private void encenderLuz(){
+       this.lucesActivas = false; 
+    }
+}
+    
