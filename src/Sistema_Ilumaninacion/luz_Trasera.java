e8e516c4f3e2a274dd61c3f3eb01b6dab371e34d
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-26 20:07:50 -0600

    Clase Luces traseras (reversa)
    Se crea la clase luz trasera con su atributo booleano protected, metodo getter y cnstructor

---
+++ src/Sistema_Ilumaninacion/luz_Trasera.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema_Ilumaninacion;
+
+/**
+ *
+ * @author Gloriana
+ */
+public class luz_Trasera {
+    //esta se implementa en sensores de reversa
+    protected boolean luzReversa;
+
+    public boolean isLuzReversa() {
+        return luzReversa;
+    }
+
+    public luz_Trasera() {
+        this.luzReversa = false;
+    }
+    
+    
+}
