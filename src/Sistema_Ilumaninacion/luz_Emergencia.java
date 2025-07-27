f595d13da378886bfe9ab946666da7ecb055bee1
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-26 19:41:26 -0600

    Clase luz emergencia
    Se crea la clase con su atributo booleano, metodo getter y  constructor

---
+++ src/Sistema_Ilumaninacion/luz_Emergencia.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema_Ilumaninacion;
+
+/**
+ *
+ * @author Gloriana
+ */
+public class luz_Emergencia {
+    private boolean luzEmegencia;
+
+    public boolean isLuzEmegencia() {
+        return luzEmegencia;
+    }
+
+    public luz_Emergencia() {
+        this.luzEmegencia = false;
+    }
+    
+    
+}
