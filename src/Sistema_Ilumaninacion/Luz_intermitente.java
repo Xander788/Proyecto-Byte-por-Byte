dc31468a93822fd87561930d4a9f12a7db462be3
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-26 19:30:46 -0600

    Clase Luces intermitentes
    Se crea la clase luces intermitentes con su atributo boleano su metodo get y constructor

---
+++ src/Sistema_Ilumaninacion/Luz_intermitente.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema_Ilumaninacion;
+
+/**
+ *
+ * @author Gloriana
+ */
+public class Luz_intermitente{
+    //aca no se usa metodo solo constructor
+    private boolean luzIntermitentes;
+
+    public boolean isLuzIntermitentes() {
+        return luzIntermitentes;
+    }
+
+    public void Luz_intermitente() {
+        this.luzIntermitentes = false;
+       
+    }
+
+    
+
+   
+    
+    
+    
+}
